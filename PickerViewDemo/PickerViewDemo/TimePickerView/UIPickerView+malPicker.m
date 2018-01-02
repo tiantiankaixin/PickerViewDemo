b2b30e5cd70a39e15d359423a3a529a38894adf1
mal <liang.ma@kunlun-inc.com>
2018-01-02 14:48:20 +0800

    ...

--- PickerViewDemo/PickerViewDemo/TimePickerView/UIPickerView+malPicker.m
+++ PickerViewDemo/PickerViewDemo/TimePickerView/UIPickerView+malPicker.m
@@ -16,7 +16,7 @@
         
         if (obj.frame.size.height < 1)
         {
-            [obj setBackgroundColor:[UIColor redColor]];
+            [obj setBackgroundColor:[UIColor clearColor]];
         }
     }];
 }
